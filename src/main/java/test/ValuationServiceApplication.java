96956b905c198f746ab65185296cfc5b1e3455fe
Pranav Kumar <pranav.kumar@iongroup.com>
2020-01-14 17:34:21 +0530

    initial commit

---
+++ src/main/java/test/ValuationServiceApplication.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuationServiceApplication.class, args);
+	}
+
+}
